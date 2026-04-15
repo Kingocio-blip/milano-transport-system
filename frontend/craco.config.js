312d2758dbd8b24107274f89c0d3f840f268c7d0
Kingocio-blip <kingocio@gmail.com>
2026-04-15 02:26:15 +0200

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,11 +2,28 @@ const path = require('path');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
+    configure: (webpackConfig, { env, paths }) => {
+      // Alias
       webpackConfig.resolve.alias = {
         ...webpackConfig.resolve.alias,
         '@': path.resolve(__dirname, 'src'),
       };
+      
+      // Desactivar minimización de CSS en producción
+      if (env === 'production') {
+        const miniCssExtractPlugin = webpackConfig.plugins.find(
+          plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+        );
+        if (miniCssExtractPlugin) {
+          miniCssExtractPlugin.options.ignoreOrder = true;
+        }
+        
+        // Desactivar CssMinimizerPlugin
+        webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
+          minimizer => minimizer.constructor.name !== 'CssMinimizerPlugin'
+        );
+      }
+      
       return webpackConfig;
     },
   },
